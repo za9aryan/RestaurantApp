80ac20bde080525ead875f15d16d42a39512f8d3
za9aryan <71818498+za9aryan@users.noreply.github.com>
2022-03-07 03:39:30 +0400

    added some front elements

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGODB: "mongodb+srv://zaqaryanalbert:Abo126984753A@cluster0.aesra.mongodb.net/testApp?retryWrites=true&w=majority",
+    MONGODB: "mongodb+srv://zaqaryanalbert:mXZcPgcP4BcZwWu4@cluster0.aesra.mongodb.net/testApp?retryWrites=true&w=majority",
     SECRET_KEY: "SOME SECRET"
 }
